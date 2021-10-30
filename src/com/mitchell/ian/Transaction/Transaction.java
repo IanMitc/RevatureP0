c56c756212b9df445786a0d43b774820222ee7a2
Ian Mitchell <sweetwaterburns@gmail.com>
2021-10-30 15:24:27 -0600

    UPdated Transaction

--- src/com/mitchell/ian/Transaction/Transaction.java
+++ src/com/mitchell/ian/Transaction/Transaction.java
@@ -9,6 +9,24 @@ public class Transaction {
     private final Account toAccount;
     private final String memo;
     private final User initiatedBy;
+    private boolean pending;
+
+    public Transaction(double amount, Account fromAccount, Account toAccount, String memo, User initiatedBy) {
+        this.amount = amount;
+        this.fromAccount = fromAccount;
+        this.toAccount = toAccount;
+        this.memo = memo;
+        this.initiatedBy = initiatedBy;
+        pending = true;
+    }
+
+    public boolean isPending() {
+        return pending;
+    }
+
+    public void setPending(boolean pending) {
+        this.pending = pending;
+    }
 
     public double getAmount() {
         return amount;
@@ -28,13 +46,5 @@ public class Transaction {
 
     public User getInitiatedBy() {
         return initiatedBy;
-    }
-
-    public Transaction(double amount, Account fromAccount, Account toAccount, String memo, User initiatedBy) {
-        this.amount = amount;
-        this.fromAccount = fromAccount;
-        this.toAccount = toAccount;
-        this.memo = memo;
-        this.initiatedBy = initiatedBy;
     }
 }
