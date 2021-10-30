044427186ed448197bccf80080382efcdd9597fb
Ian Mitchell <sweetwaterburns@gmail.com>
2021-10-30 15:18:55 -0600

    Implemented Transaction

--- src/com/mitchell/ian/Transaction/Transaction.java
+++ src/com/mitchell/ian/Transaction/Transaction.java
@@ -1,4 +1,40 @@
 package com.mitchell.ian.Transaction;
 
+import com.mitchell.ian.Account.Account;
+import com.mitchell.ian.User.User;
+
 public class Transaction {
+    private final double amount;
+    private final Account fromAccount;
+    private final Account toAccount;
+    private final String memo;
+    private final User initiatedBy;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Account getFromAccount() {
+        return fromAccount;
+    }
+
+    public Account getToAccount() {
+        return toAccount;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public User getInitiatedBy() {
+        return initiatedBy;
+    }
+
+    public Transaction(double amount, Account fromAccount, Account toAccount, String memo, User initiatedBy) {
+        this.amount = amount;
+        this.fromAccount = fromAccount;
+        this.toAccount = toAccount;
+        this.memo = memo;
+        this.initiatedBy = initiatedBy;
+    }
 }
