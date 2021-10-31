d88114b05977d3f9dfcc6f833e783afd02b26806
Ian Mitchell <sweetwaterburns@gmail.com>
2021-10-31 12:51:12 -0600

    Borrowed code to clear the console for UI

--- src/com/mitchell/ian/TheBank.java
+++ src/com/mitchell/ian/TheBank.java
@@ -1,7 +1,12 @@
 package com.mitchell.ian;
 
+import com.mitchell.ian.UI.Utility.Clear;
+
 public class TheBank {
 
     public static void main(String[] args) {
+        System.out.println("Helloooooo");
+
+        Clear.console();
     }
 }
---
+++ src/com/mitchell/ian/UI/Utility/Clear.java
@@ -0,0 +1,26 @@
+package com.mitchell.ian.UI.Utility;
+
+// clear console method borrowed from https://www.delftstack.com/howto/java/java-clear-console/
+public class Clear {
+
+    public static void console() {
+        try {
+            String operatingSystem = System.getProperty("os.name"); //Check the current operating system
+
+            if (operatingSystem.contains("Windows")) {
+                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
+                Process startProcess = pb.inheritIO().start();
+                startProcess.waitFor();
+            } else {
+                ProcessBuilder pb = new ProcessBuilder("clear");
+                Process startProcess = pb.inheritIO().start();
+
+                startProcess.waitFor();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+
