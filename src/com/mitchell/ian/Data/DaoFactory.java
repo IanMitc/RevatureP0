22538e0149fc9afe3a6dfaaf087c846adf392e50
Ian Mitchell <sweetwaterburns@gmail.com>
2021-11-01 16:48:57 -0600

    Add User2AccountDao to DaoFactory

--- src/com/mitchell/ian/Data/DaoFactory.java
+++ src/com/mitchell/ian/Data/DaoFactory.java
@@ -1,11 +1,18 @@
 package com.mitchell.ian.Data;
 
 public class DaoFactory {
-    private static UserDao userDao = null;
     private static AccountDao accountDao = null;
+    private static UserDao userDao = null;
+    private static User2AccountDao user2AccountDao = null;
     private static TransactionDao transactionDao = null;
 
     private DaoFactory() {
+    }
+
+    public static AccountDao getAccountDao() {
+        if (accountDao == null)
+            accountDao = new AccountDaoImpl();
+        return accountDao;
     }
 
     public static UserDao getUserDao() {
@@ -14,10 +21,10 @@ public class DaoFactory {
         return userDao;
     }
 
-    public static AccountDao getAccountDao() {
-        if (accountDao == null)
-            accountDao = new AccountDaoImpl();
-        return accountDao;
+    public static User2AccountDao getUser2AccountDao() {
+        if (user2AccountDao == null)
+            user2AccountDao = new User2AccountDaoImpl();
+        return user2AccountDao;
     }
 
     public static TransactionDao getTransactionDao() {
