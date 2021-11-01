c0006a255f7f3169cddabb9379af87745c74a950
Ian Mitchell <sweetwaterburns@gmail.com>
2021-11-01 16:34:05 -0600

    DaoFactory as Singleton fixes

--- src/com/mitchell/ian/Data/DaoFactory.java
+++ src/com/mitchell/ian/Data/DaoFactory.java
@@ -1,15 +1,15 @@
 package com.mitchell.ian.Data;
 
-import com.mitchell.ian.Transaction.Transaction;
-import com.mitchell.ian.User.User;
-
 public class DaoFactory {
     private static UserDao userDao = null;
     private static AccountDao accountDao = null;
     private static TransactionDao transactionDao = null;
 
-    public static UserDao getUserDao(){
-        if(userDao == null)
+    private DaoFactory() {
+    }
+
+    public static UserDao getUserDao() {
+        if (userDao == null)
             userDao = new UserDaoImpl();
         return userDao;
     }
