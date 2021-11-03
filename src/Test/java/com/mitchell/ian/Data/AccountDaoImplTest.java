4d4a8bb1dfd29da63ad2e53847e495057867dc11
Ian Mitchell <sweetwaterburns@gmail.com>
2021-11-03 08:35:07 -0600

    Add JUnit

---
+++ src/Test/java/com/mitchell/ian/Data/AccountDaoImplTest.java
@@ -0,0 +1,47 @@
+package com.mitchell.ian.Data;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountDaoImplTest {
+
+    @BeforeEach
+    void setUp() {
+        Database.initialize();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAccount() {
+    }
+
+    @Test
+    void getAllAccounts() {
+    }
+
+    @Test
+    void testGetAllAccounts() {
+    }
+
+    @Test
+    void getAllPendingAccounts() {
+    }
+
+    @Test
+    void addAccount() {
+    }
+
+    @Test
+    void deleteAccount() {
+    }
+
+    @Test
+    void updateAccount() {
+    }
+}
